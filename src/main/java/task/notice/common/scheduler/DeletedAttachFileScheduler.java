f0e70c5b3536ef1c7a8903da9c26a4bfbfb8a82a
kimdh-hi <zbeld123@gmail.com>
2022-01-31 12:42:22 +0900

    첨부파일 삭제는 soft delete로 하고 스케줄러에 의해 DB 및 S3 데이터 삭제하도록 처리 (#8)

--- src/main/java/task/notice/common/scheduler/DeletedAttachFileScheduler.java
+++ src/main/java/task/notice/common/scheduler/DeletedAttachFileScheduler.java
@@ -1,6 +1,7 @@
 package task.notice.common.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import task.notice.attachfile.domain.AttachFile;
@@ -9,6 +10,7 @@ import task.notice.common.aws.AwsS3Utils;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class DeletedAttachFileScheduler {
@@ -19,6 +21,7 @@ public class DeletedAttachFileScheduler {
     @Scheduled(cron = "0 0 3 * * ?")
     public void job() {
         List<AttachFile> deletedAttachFiles = attachFileRepository.findByDeleted(true);
+        log.info("DeletedAttachFileScheduler delete file size = {}", deletedAttachFiles.size());
         deletedAttachFiles.forEach(
                 f -> {
                     s3Utils.delete(f.getSaveFileName());
--- src/main/java/task/notice/notice/dto/response/AttachFileResponseDto.java
+++ src/main/java/task/notice/notice/dto/response/AttachFileResponseDto.java
@@ -10,10 +10,11 @@ import task.notice.attachfile.domain.AttachFile;
 @Data
 public class AttachFileResponseDto {
 
+    private Long attachFileId;
     private String url;
     private String fileName;
 
     public static AttachFileResponseDto of(AttachFile attachFile) {
-        return new AttachFileResponseDto(attachFile.getFileUrl(), attachFile.getOriginalFileName());
+        return new AttachFileResponseDto(attachFile.getId(), attachFile.getFileUrl(), attachFile.getOriginalFileName());
     }
 }
